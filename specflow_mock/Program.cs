80c8f3263dcc50cc0eb86afc0353495993f9183e
Tom Gregor <tgregor@ivsoftware.com>
2022-07-21 08:13:55 -0600

    Add project files.

---
+++ specflow_mock/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using TechTalk.SpecFlow;
+
+namespace specflow_mock
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Normal dictionary
+            var normal = new Dictionary<string, object>
+            {
+                { "Apple", "Red" },
+                { "Orange", "Orange" },
+            };
+            // Calls CollectionExtensions.GetValueOrDefault()
+            Console.WriteLine(normal.GetValueOrDefault("Apple"));
+
+            // SpecFlowContext dictionary
+            var fake = new CustomSpecFlowContext
+            {
+                { "Apple", "Red" },
+                { "Orange", "Orange" },
+            };
+            Console.WriteLine(fake.GetValueOrDefault("Apple"));
+            Console.WriteLine(fake.GetValueOrDefault("Apple", (string)null));
+            Console.WriteLine(fake.GetValueOrDefault("Apple", "Fake value C"));
+        }
+    }
+
+    class CustomSpecFlowContext : SpecFlowContext { }
+
+    static class Extensions
+    {
+        public static object GetValueOrDefault<TKey>(
+            this SpecFlowContext dictionary, 
+            TKey key) where TKey : notnull
+        {
+            return "Fake value A";
+        }
+
+        public static object GetValueOrDefault<TKey, TValue>(
+            this SpecFlowContext dictionary, 
+            TKey key, 
+            [AllowNull] TValue defaultValue)
+        {
+            if(defaultValue is string valid)
+            {
+                return valid;
+            }
+            return "Fake value B";
+        }
+    }
+}
